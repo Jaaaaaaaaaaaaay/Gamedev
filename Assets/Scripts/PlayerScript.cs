a3f97f79f03ad4e678061023b58a06b5106943e6
Jaaaaaaaaaaaaay <jaymob368@gmail.com>
2023-10-30 02:57:31 +0800

    Newcommit
    wtf

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private float horizontal;
+    private float vertical;
+    private float speed=4.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(horizontal*speed, vertical*speed);
     }
 }
